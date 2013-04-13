c21db48b5306656edbbbe24b6ec0a00f0a700341
Jan Zieschang <dev@zieschang.net>
2013-04-13 18:05:37 +0200

    JC-10 #time 1h #resolve #comment Extracted the code to make field inline editable, added loading indicator for personal page
    Signed-off-by: Jan Zieschang <dev@zieschang.net>

---
+++ src/JiraClient/JiraClient.WebMVC/Scripts/helper-code.js
@@ -0,0 +1,18 @@
+﻿function RegisterForInlineEditing(ctrl) {
+    $(ctrl).click(function () {
+        var userName = $(this).text();
+        var input = $("<input type='text' value='" + userName + "'/>");
+        $(this).empty().append($(input));
+        $(input).select();
+        $(input).focusout(function () {
+            var userName = $(this).val();
+            $(this).parent().empty().text(userName);
+        });
+        //register ...
+    });
+    $(ctrl).hover(function () {
+        $(this).addClass("hover");
+    }, function () {
+        $(this).removeClass("hover");
+    });
+}
