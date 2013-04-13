504b271230be961305ceae71cd86f723d9a6c74e
Jan Zieschang <dev@zieschang.net>
2013-04-13 18:54:58 +0200

    * fixed an error for the date formatter (missing) * some preperation for JC-8
    Signed-off-by: Jan Zieschang <dev@zieschang.net>

--- src/JiraClient/JiraClient.WebMVC/Scripts/helper-code.js
+++ src/JiraClient/JiraClient.WebMVC/Scripts/helper-code.js
@@ -1,7 +1,12 @@
 ﻿function RegisterForInlineEditing(ctrl) {
-    $(ctrl).click(function () {
+    RegisterForInlineEditingItem($(ctrl));
+}
+function RegisterForInlineEditingItem(ctrlAsElement, formatting) {
+    ctrlAsElement.click(function () {
         var userName = $(this).text();
         var input = $("<input type='text' value='" + userName + "'/>");
+        if(formatting != null)
+            $(input).addClass(formatting);
         $(this).empty().append($(input));
         $(input).select();
         $(input).focusout(function () {
@@ -9,10 +14,17 @@
             $(this).parent().empty().text(userName);
         });
         //register ...
-    });
-    $(ctrl).hover(function () {
+    }).hover(function () {
         $(this).addClass("hover");
     }, function () {
         $(this).removeClass("hover");
     });
+
+}
+
+
+function FormatDate(jsonDate) {
+    var value = new Date(jsonDate);
+    //alert(value.toISOString("HH:mm DD.MM.YYYY"));
+    return value.toLocaleString();  //value.getHours() + ":" + value.getMinutes() + " " + value.getDay() + "." + (value.getMonth()+1) + "." + value.getYear();
 }
